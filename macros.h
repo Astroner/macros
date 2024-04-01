9d71d907c2daa044d5c119bbf765a294ecd6b0e0
Eugene Astroner <zhenyanikforov@gmail.com>
2024-04-02 01:45:04 +0300

    DEFER update

--- macros.h
+++ macros.h
@@ -1,8 +1,11 @@
 #if !defined(DEFER)
-#define DEFER
 
 #define DEFER(start, end) for(start, *i = 0x0; i != (void*)0x1; i = (end, (void*)0x1)) 
+
+// Exit DEFER and run cleanup
 #define DEFER_EXIT continue;
+
+// EXIT DEFER with no cleanup
 #define DEFER_BREAK break;
 
 #endif // DEFER
